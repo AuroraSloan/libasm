1ddaca58c1b637f5e7cc9bd9b7d8efc39662c66e
AuroraSloan <lanajadethompson@gmail.com>
2024-11-16 00:29:53 +0900

    use macro

--- ft_list_size_bonus.s
+++ ft_list_size_bonus.s
@@ -5,6 +5,8 @@ section .note.GNU-stack
 section .text
 global ft_list_size
 
+%define t_list_next_offset 8
+
 ft_list_size:
 	xor rax, rax
 	mov r8, rdi
@@ -13,7 +15,7 @@ next:
 	cmp r8, 0
 	je ret
 	inc rax
-	mov r8, [r8 + 8]
+	mov r8, [r8 + t_list_next_offset]
 	jmp next
 
 ret:
