294f1aba6727443e2117323149341c123be47483
AuroraSloan <lanajadethompson@gmail.com>
2024-11-18 00:16:51 +0900

    update sort WIP

--- ft_list_sort_bonus.s
+++ ft_list_sort_bonus.s
@@ -12,21 +12,32 @@ global ft_list_sort
 ;r9 cmp func
 ;r10 rdi's prev
 ;r11 rsi's prev
+;r12 rdi node
+;r13 rsi node
 ;rdx tmp
 
 ft_list_sort:
+	test rdi, rdi
+	jz ret
+	test rsi, rsi
+	jz ret
+	push r12
+	push r13
+
 	mov r8, rdi ;head
-	mov r9, rsi ;cmp	
+	mov r9, rsi ;cmp
 	xor r10, r10
 
-	cmp qword [r8], 0
-	je ret
-	mov rdi, [r8]
+	mov r12, [r8]
+	test r12, r12
+	jz ret
+	lea rdi, [r12 + t_list_data_offset]
 
-	cmp qword [rdi + t_list_next_offset], 0
-	je ret
-	mov r11, rdi
-	mov rsi, [rdi + t_list_next_offset]
+	mov r13, [rdi + t_list_next_offset]
+	test r13, r13
+	jz ret
+	mov r11, r12
+	lea rsi, [r13 + t_list_data_offset]
 
 insert_sort:
 	call r9
@@ -34,39 +45,44 @@ insert_sort:
 	jng next_cmp
 
 	; rsi to tmp rdx and set new rsi
-	mov rdx, rsi
-	mov rsi, [rsi + t_list_next_offset]
-	mov [r11 + t_list_next_offset], rsi
+	mov rdx, r13
+	mov r13, [r13 + t_list_next_offset]
+	mov [r11 + t_list_next_offset], r13
 
 	; set rdx before rdi
-	mov [rdx + t_list_next_offset], rdi
-	test r10, 0
-	jmp update_head
+	mov [rdx + t_list_next_offset], r12
+	test r10, r10
+	jz update_head
 	mov [r10 + t_list_next_offset], rdx
 
 	jmp reset
 
 next_cmp:
-	test [rdi + t_list_next_offset], rsi
-	jz next_node
-	mov r10, rdi
-	mov rdi, [rdi + t_list_next_offset]
+	cmp [r12 + t_list_next_offset], r13
+	je next_node
+	mov r10, r12
+	mov r12, [r12 + t_list_next_offset]
+	lea rdi, [r12 + t_list_data_offset]
 	jmp insert_sort
 
 next_node:
-	mov r11, rsi
-	mov rsi, [rsi + t_list_next_offset]
+	mov r11, r13
+	mov r13, [r13 + t_list_next_offset]	
 	jmp reset
 
 update_head:
 	mov [r8], rdx
 
 reset:
-	test rsi, 0
+	test r13, r13
 	jz ret
+	lea rsi, [r13 + t_list_data_offset]
 	xor r10, r10
-	mov rdi, [r8]
+	mov r12, [r8]
+	lea rdi, [r12 + t_list_data_offset]
 	jmp insert_sort
 
 ret:
+	pop r13
+	pop r12
 	ret
