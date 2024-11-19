8c864c0fa65910afe33a890f8c4f52c2acb8dc82
AuroraSloan <lanajadethompson@gmail.com>
2024-11-20 00:37:13 +0900

    fix list remove

--- ft_list_remove_if_bonus.s
+++ ft_list_remove_if_bonus.s
@@ -9,34 +9,66 @@ global ft_list_remove_if
 %define t_list_next_offset 8
 
 ft_list_remove_if:
-	mov r8, rdi ;head
-
-cmp_head:
-	mov rdi, [r8]
 	test rdi, rdi
 	jz ret
-	call rdx
-	test rax, rax
-	jnz cmp_next
-	mov r9, [rdi + t_list_next_offset]
-	mov [r8], r9
-	call rcx
-	jmp cmp_head	
-
-cmp_next:
-	mov r11, rdi
-	mov rdi, [rdi + t_list_next_offset]
-	test rdi, rdi
+	test rsi, rsi
 	jz ret
-	call rdx
-	test rax, rax
-	jnz cmp_next
+	test rdx, rdx
+	jz ret
 
-	mov r9, [rdi + t_list_next_offset]
-	mov [r11 + t_list_next_offset], r9
-	call rcx
-	mov rdi, r11
-	jmp cmp_next
+	push rbx
+	push r12
+	push r13
+	push r14
+	push r15
+	push rbp
+
+	mov rbx, rdi ;head
+	mov r12, rsi ;data
+	mov r13, rdx ;cmp
+	mov r14, rcx ;free
+	mov r15, [rbx] ;node
+	xor rbp, rbp ;prev
+
+	mov rdi, [r15 + t_list_data_offset]
+
+cmp:
+	call r13
+	test eax, eax
+	jnz get_next
+
+	mov rdi, r15
+	mov r15, [r15 + t_list_next_offset]
+	test rbp, rbp
+	jz set_head
+	mov [rbp + t_list_next_offset], r15
+	jmp ft_free
+
+set_head:
+	mov [rbx], r15
+
+ft_free:
+	call r14
+	jmp check_node
+
+get_next:
+	mov rbp, r15
+	mov r15, [r15 + t_list_next_offset]
+
+check_node:
+	test r15, r15
+	jz fin
+	mov rdi, [r15 + t_list_data_offset]
+	mov rsi, r12
+	jmp cmp
+
+fin:
+	pop rbp
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbx
 
 ret:
 	ret
